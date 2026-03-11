331c70595beebe7ef60f9506a943fee34cb60ac8
adaatidev <ada.atienza@iesnervion.es>
2026-03-11 19:18:34 +0100

    Segundo boletín de poo completado

---
+++ src/boletin02/Libro.java
@@ -0,0 +1,118 @@
+package boletin02;
+
+public class Libro {
+
+	String titulo;
+	String autor;
+	int ejemplares;
+	int prestados;
+
+	enum Generos {
+		Narrativo, Lírico, Dramático, Didáctico, Poético
+	};
+
+	Generos genero;
+
+	// CONSTRUCTORES
+	Libro(String titulo, String autor) {
+		this.titulo = titulo;
+		this.autor = autor;
+	}
+
+	Libro(String titulo, String autor, int ejemplares, int prestados) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.ejemplares = ejemplares;
+		this.prestados = prestados;
+	}
+
+	Libro(String titulo, String autor, int ejemplares, int prestados, Generos genero) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.ejemplares = ejemplares;
+		this.prestados = prestados;
+		this.genero = genero;
+	}
+
+	// GETTERS/SETTERS
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getEjemplares() {
+		return ejemplares;
+	}
+
+	public void setEjemplares(int ejemplares) {
+		this.ejemplares = ejemplares;
+	}
+
+	public int getPrestados() {
+		return prestados;
+	}
+
+	public void setPrestados(int prestados) {
+		this.prestados = prestados;
+	}
+
+	public Generos getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Generos genero) {
+		this.genero = genero;
+	}
+
+	// MÉTODOS
+	public boolean prestamo(int ejemplares) {
+		boolean valido = false;
+
+		if (this.ejemplares > 0) {
+			this.ejemplares--;
+			this.prestados++;
+			valido = true;
+		}
+
+		return valido;
+	}
+
+	public boolean devolucion(int ejemplares, int prestados) {
+		boolean seDevuelve = false;
+
+		if (this.prestados > 0) {
+			this.ejemplares++;
+			this.prestados--;
+			seDevuelve = true;
+		}
+
+		return seDevuelve;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Libro [Título: %s, Autor: %s, Ejemplares: %s, Prestados: %s, Género: %s]", titulo, autor,
+				ejemplares, prestados, genero);
+	}
+
+	public boolean equals(String titulo1, String titulo2, String autor1, String autor2) {
+		boolean iguales = false;
+
+		if (titulo1.equals(titulo2) && autor1.equals(autor2)) {
+			iguales = true;
+		}
+
+		return iguales;
+	}
+}
---
+++ src/boletin02/LibroMain.java
@@ -0,0 +1,19 @@
+package boletin02;
+
+import boletin02.Libro.Generos;
+
+public class LibroMain {
+
+	public static void main(String[] args) {
+
+		Libro libro1 = new Libro("El Quixote", "Miguel de Cervantes", 20, 10, Generos.Dramático);
+		Libro libro2 = new Libro("Diccionario", "Yo Obviamente", 0, 888, Generos.Didáctico);
+
+		System.out.println(libro1.prestamo(libro1.ejemplares));
+		libro1.devolucion(libro1.ejemplares, libro1.prestados);
+		System.out.println(libro1.toString());
+		System.out.println(libro1.equals(libro2));
+
+	}
+
+}
